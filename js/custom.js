6fdef0bfdebbb49a6f924d42870e48eeee5ab4fd
Diana Michelle <geekmlle@gmail.com>
2016-03-07 19:01:40 -0500

    Changes to the Pace Mobile Lab Website
    Covered the following:
    
    Menu:
    Home / Research / Courses / Events / Team / Contact Us
    
    Github to be added in contact
    https://github.com/paceuniversity/pacemobilelab
    
    Order:
    Intro
    Highlighted event
    Research (to correct)
    Courses
    Publications (to add)
    Events
    Team
    Contact us
    
    Look and feel:
    Some pitcutres in the next email.
    Less purple
    Logo at the top also
    Pace Mobile Lab instead of Welcome to ...
    
    Research
    App Dock (a link to APPDOCKPROJECT.COM)
    Beacons
    MobileSeengal (http://mobilesenegal.org)
    Data for Development
    Wearables
    Everyday Glass
    
    Courses
    CS 369
    Software Engineering
    
    CS 641
    Mobile Web & Content
    
    CS 643
    Mobile Innovations for Global Challenges
    
    CS 639
    Mobile Application Development
    
    Events
    MIT App Inventor for CIT 110 students
    Android Crash Course
    Pace Entrepreneurship Lab Mobile Design Contest
    MIT App Inventor for ChickTech
    Google IO Extended 2015
    
    Member
    Jan Schoepp Graduate assistant
    Meghan Jordan Graduate assistant (past)
    Pooja Mahesh Graduate assistant (past)
    Nish is Graduate student

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,34 @@ $('#siteNav').affix({
 	offset: {
 		top: 100
 	}
-})
+});
+
+var navHolder = $(".collapse ul.nav");
+var pathname = window.location.hash;
+var completePath = window.location.href;
+
+navHolder.find("li").each(function(){
+	var el = $(this);
+	el.on("click", function(){
+		navHolder.find("li.active").removeClass("active");
+		el.addClass("active");
+	});
+});
+
+navHolder.find("li a").each(function(){
+	if (completePath.indexOf("index") > 1){
+		var txt;
+		if (!pathname){
+			pathname = '#';
+			txt = '#';
+		} else {
+			txt = '#'+$(this).text();
+		}
+		txt = txt.replace(/\s+/g, '-').toLowerCase();
+		if (pathname.toUpperCase() === txt.toUpperCase()){
+			$(this).parent().addClass("active");
+		} else {
+			$(this).parent().removeClass("active");
+		}
+	}
+});
